292ef62b974e661ca1109720950a6e86d7ae7f4d
Iván Alejandro <ivanali@outlook.com>
2016-01-23 11:44:47 -0600

    Handlebars view engine basic functionality integrated

--- meadowlark.js
+++ meadowlark.js
@@ -1,29 +1,30 @@
 var express = require('express');
 var app = express();
 
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(request, response) {
-    response.type('text/plain');
-    response.send('Meadowlark Travel');
+    response.render('home');
 });
 
 app.get('/about', function(request, response) {
-    response.type('text/plain');
-    response.send('About Meadowlark Travel');
+    response.render('about');
 });
 
 app.use(function(request, response) {
-    response.type('text/plain');
     response.status(404);
-    response.send('404 - Not found');
+    response.render('404');
 });
 
 app.use(function(error, request, response, next) {
     console.error(error.stack);
-    response.type('text/plain');
     response.status(500);
-    response.send('500 - Server error');
+    response.render('500');
 });
 
 app.listen(app.get('port'), function() {
