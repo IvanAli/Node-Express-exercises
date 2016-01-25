a02ab9bfe36f4d20d7fa1b62849454e4f6cfc014
Iván Alejandro <ivanali@outlook.com>
2016-01-25 14:10:50 -0600

    Chapter 5 done. Some incompatibilities with the API of the version of Zombie.js used

--- meadowlark.js
+++ meadowlark.js
@@ -10,6 +10,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(request, response, next) {
+    response.locals.showTests = app.get('env') !== 'production' && request.query.test === '1';
+    next();
+});
 
 app.get('/', function(request, response) {
     response.render('home');
@@ -17,7 +21,22 @@ app.get('/', function(request, response) {
 
 app.get('/about', function(request, response) {
     var randFortune = fortune.getFortune();
-    response.render('about', {fortune: randFortune});
+    response.render('about', {
+        fortune: randFortune,
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+
+app.get('/tours/hood-river', function(request, response) {
+    response.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function(request, response) {
+    response.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function(request, response) {
+    response.render('tours/request-group-rate');
 });
 
 app.use(function(request, response) {
